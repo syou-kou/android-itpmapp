46252b3898168e3506bf11cb8a63a162018b9d0d
syoukou <fatman01221087@gmail.com>
2019-12-07 19:04:22 +0900

    2nd commit

--- app/src/main/java/com/example/itpm_app/activities/MainActivity.java
+++ app/src/main/java/com/example/itpm_app/activities/MainActivity.java
@@ -1,7 +1,8 @@
-package com.example.itpm_app;
+package com.example.itpm_app.activities;
 
 import android.os.Bundle;
 
+import com.example.itpm_app.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -29,27 +30,5 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/itpm_app/activities/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.itpm_app.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.itpm_app.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
